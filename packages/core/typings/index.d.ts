8157cc9205a71ba9190b50e446b7470c223548a5
Wang Yucai <netowls-studio@outlook.com>
2023-08-07 16:13:46 +0800

    feat: 删除不合理的代码。

--- packages/core/typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-// *******************************************************************************************************************************************************
-// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
-// COPYRIGHT ? 2023 BEIJING JOINGO.VIP INFORMATION TECHNOLOGY CO., LTD. ALL RIGHTS RESERVED.
-// *******************************************************************************************************************************************************
-
-/// <reference path="./lib.types.d.ts" />
-/// <reference path="./lib.native.d.ts" />
