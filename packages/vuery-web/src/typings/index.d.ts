de9de05534d34592ffbfdaf89528743c3dc7e98a
Wang Yucai <netowls-studio@outlook.com>
2023-08-09 16:36:07 +0800

    feat: 调整了 Vite 环境变量类型定义目录结构。

---
+++ packages/vuery-web/src/typings/index.d.ts
@@ -0,0 +1,15 @@
+// *******************************************************************************************************************************************************
+// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+// COPYRIGHT © 2023 BEIJING JOINGO.VIP INFORMATION TECHNOLOGY CO., LTD. ALL RIGHTS RESERVED.
+// *******************************************************************************************************************************************************
+
+/**
+ * Vuery Desktop 私有类型扩展模块。
+ *
+ * @remarks
+ * "index.d.ts": Vuery Desktop 私有类型扩展模块。
+ *
+ * @packageDocumentation
+ */
+
+/// <reference path="lib.vite.d.ts" />
---
+++ packages/vuery-web/src/typings/lib.vite.d.ts
@@ -0,0 +1,15 @@
+// *******************************************************************************************************************************************************
+// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+// COPYRIGHT © 2023 BEIJING JOINGO.VIP INFORMATION TECHNOLOGY CO., LTD. ALL RIGHTS RESERVED.
+// *******************************************************************************************************************************************************
+
+/**
+ * Vite 环境变量定义模块。
+ *
+ * @remarks
+ * "lib.vite.d.ts": Vite 环境变量定义模块。
+ *
+ * @packageDocumentation
+ */
+
+/// <reference types="vite/client" />
