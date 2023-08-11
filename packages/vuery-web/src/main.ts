d131fbc49d5cc480b5c9dbd3948f9d2e709a44ab
Wang Yucai <netowls-studio@outlook.com>
2023-08-11 12:16:01 +0800

    feat: 调整了 Vuery 应用程序入口方法。

--- packages/vuery-web/src/main.ts
+++ packages/vuery-web/src/main.ts
@@ -12,35 +12,23 @@
  * @packageDocumentation
  */
 
-import { RuntimeExtensions } from '@vuery/runtime';
+import { AppBuilder } from '@vuery/runtime';
 import { App, createApp } from 'vue';
 import Main from './Main.vue';
 import ElementPlus from 'element-plus';
 import '@vuery/native';
 
-/**
- * 配置应用程序。
- * @author Wang Yucai
- *
- * @param {App} app {@link App} 类型的对象实例。
- */
-function configureApp(app: App): void {
-  new RuntimeExtensions(app)
-    .useExtensions()
-    .use((_app) => {
-      console.debug(`[DEBUG] - <main.ts: aedfad>: 尝试注册样式表。`);
-      import('animate.css/animate.css');
-      import('@mdi/font/css/materialdesignicons.css');
-      import('@/assets/overrides/element-plus.scss');
-      import('@/assets/index.scss');
-    })
-    .use((_app) => {
-      console.debug(
-        `[DEBUG] - <main.ts: c44ac1>: 尝试注册 Element-Plus 组件库。`
-      );
-      _app.use(ElementPlus, { size: 'default', 'zIndex': 2023 });
-    });
-  app.mount('body');
-}
-
-configureApp(createApp(Main));
+new AppBuilder(createApp(Main), 'body')
+  .configureBaseUri(import.meta.env.BASE_URL)
+  .use(() => {
+    console.debug(`[DEBUG] - <main.ts: 00160e>: 注册全局样式表。`);
+    import('@mdi/font/css/materialdesignicons.css');
+    import('animate.css/animate.css');
+    import('@/assets/overrides/element-plus.scss');
+    import('@/assets/index.scss');
+  })
+  .use((app) => {
+    app.use(ElementPlus, { size: 'default', 'zIndex': 2023 });
+  })
+  .build()
+  .run();
