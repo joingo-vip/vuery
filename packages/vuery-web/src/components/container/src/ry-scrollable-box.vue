dd0ba532053f49f7a41d6884ff2fb325175c33f6
Wang Yucai <netowls-studio@outlook.com>
2023-08-12 15:35:45 +0800

    feat: 新增可滚动容器组件。
    未完。

---
+++ packages/vuery-web/src/components/container/src/ry-scrollable-box.vue
@@ -0,0 +1,15 @@
+<!--
+  // *******************************************************************************************************************************************************
+  // LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION. 
+  // COPYRIGHT © 2023 BEIJING JOINGO.VIP INFORMATION TECHNOLOGY CO., LTD. ALL RIGHTS RESERVED.
+  // *******************************************************************************************************************************************************
+-->
+
+<!--ry-scrollable-box.vue: 可滚动的容器组件-->
+<template>
+  <div class="ry-scrollbar-box">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
