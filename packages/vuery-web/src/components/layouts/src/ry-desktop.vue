d19c45d0acdc792d10c361922506c049902b01a4
Wang Yucai <netowls-studio@outlook.com>
2023-08-22 14:37:44 +0800

    feat: 新增 Desktop 布局组件。Issues #2

--- packages/vuery-web/src/components/layouts/src/ry-desktop.vue
+++ packages/vuery-web/src/components/layouts/src/ry-desktop.vue
@@ -9,13 +9,40 @@
 <template>
   <div class="ry-desktop">
     <FlexBoxWidget direction="column" class="ry-h--100p">
-      <div class="ry-desktop-header">
-        <div></div>
+      <div class="ry-desktop-header" v-show="hasHeaderSlot">
+        <div>
+          <slot name="header" />
+        </div>
+      </div>
+      <div class="ry-desktop-body">
+        <div>
+          <slot />
+        </div>
+      </div>
+      <div class="ry-desktop-footer" v-show="hasFooterSlot">
+        <div>
+          <slot name="footer" />
+        </div>
       </div>
     </FlexBoxWidget>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { getCurrentInstance } from 'vue';
 import { FlexBoxWidget } from '../../container';
+import { computed } from 'vue';
+
+/**
+ * Vue 应用程序上下文信息。
+ */
+const $context = getCurrentInstance();
+
+const hasHeaderSlot = computed<boolean>(() => {
+  return !Object.isNull($context?.proxy?.$slots['header']);
+});
+
+const hasFooterSlot = computed<boolean>(() => {
+  return !Object.isNull($context?.proxy?.$slots['footer']);
+});
 </script>
