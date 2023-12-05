ad751d64d078f3d500c7027ffcb994da809f08d7
Wang Yucai <netowls-studio@outlook.com>
2023-12-05 09:40:34 +0800

    feat(#8): 新增公共样式表。

--- applications/web-application/src/Home.vue
+++ applications/web-application/src/Home.vue
@@ -1,3 +1,15 @@
+<!--
+  // *******************************************************************************************************************************************************
+  // LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION. 
+  // COPYRIGHT © 2023 BEIJING JOINGO.VIP INFORMATION TECHNOLOGY CO., LTD. ALL RIGHTS RESERVED.
+  // *******************************************************************************************************************************************************
+-->
+
+<!--Home.vue: 组件-->
 <template>
   <div></div>
 </template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
